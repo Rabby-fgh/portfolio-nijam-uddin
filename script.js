4baf30da4016656e76cd5571e7b65188d7dfa088
Nijam Uddin <84635901+Rabby-fgh@users.noreply.github.com>
2026-07-23 23:39:41 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+window.addEventListener("load", () => {
+    document.body.style.opacity = "1";
+});
 
+const words = [
+    "Textile Engineer",
+    "Web3 Builder",
+    "Arc Contributor"
+];
+
+let i = 0;
+
+setInterval(() => {
+    document.querySelector(".content h2").innerHTML = words[i];
+    i = (i + 1) % words.length;
+}, 2000);
